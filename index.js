822d99ca0c8d47c0540eea884d15190ba70cc75d
Nick Virga <nsvirga@gmail.com>
2023-10-02 00:28:58 -0400

    change CORS to allow all origins instead of a specific origin (temporarily)

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//   })
+// );
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -17,14 +19,14 @@ const agentsRoutes = require("./routes/agents-routes");
 const ticketsRoutes = require("./routes/tickets-routes");
 const usersRoutes = require("./routes/users-routes");
 const organizationRoutes = require("./routes/organizations-routes");
-const inquiryOptionsRoutes = require ("./routes/inquiry_options-routes")
+const inquiryOptionsRoutes = require("./routes/inquiry_options-routes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/agents", agentsRoutes);
 app.use("/api/tickets", ticketsRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/organizations", organizationRoutes);
-app.use("/api/inquiry-options", inquiryOptionsRoutes)
+app.use("/api/inquiry-options", inquiryOptionsRoutes);
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
